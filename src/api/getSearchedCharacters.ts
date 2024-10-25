c387833974eac1496dafa357604c6fa3a2aff833
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2024-10-25 19:46:29 -0400

    complete project

--- src/api/getSearchedCharacters.ts
+++ src/api/getSearchedCharacters.ts
@@ -3,7 +3,6 @@ import fetchHandler from "./fetchHandler";
 
 export default async function getSearchedCharacters(nameParam: string): Promise<{characters: CharacterType[]}> {
   const { data } = await fetchHandler(`https://api.disneyapi.dev/character?name=${nameParam}`);
-  // todo type properly
   data.sort((a: CharacterType, b: CharacterType) => b.films.length - a.films.length);
   return { characters: data };
 }
