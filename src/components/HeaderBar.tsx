b0dd4cb7fb44deb7abf25b3b259a9a43317928c2
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2024-10-25 00:27:08 -0400

    add svgs

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom"
 import SearchForm from "./SearchForm";
+import DisneyLogo from "./util-components/DisneyLogo";
+import UserLogo from "./util-components/UserLogo";
 
 export default function NavBar() {
   return (
     <header>
-      <nav>
-        <Link to="/">DISNEY</Link>
-        <Link to="/profile">User Profile</Link>
-      </nav>
+      <Link to="/"><DisneyLogo /></Link>
       <SearchForm />
+      <Link to="/profile"><UserLogo /></Link>
     </header>
   );
 }
--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import HeaderBar from "./HeaderBar";
+import DisneyLogo from "./util-components/DisneyLogo";
 
 export default function MainContainer() {
   return <>
@@ -7,6 +8,10 @@ export default function MainContainer() {
     <main>
       <Outlet />
     </main>
-    <footer>DISNEY BUT ON BOTTOM</footer>
+    <footer>
+      <DisneyLogo />
+      <p>For educational use only. All characters and content are the property of Disney. This test is for private use and development testing only and should not be distributed for public consumption</p>
+    </footer>
   </>
+
 }
